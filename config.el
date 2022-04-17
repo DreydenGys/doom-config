d02a787cc2fd14e9ad799af447fc90850365634b
dreydengys <busterkeel59@gmail.com>
2022-04-17 17:30:17 +0200

    17/04/22 config sync

--- config.el
+++ config.el
@@ -25,7 +25,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-vibrant)
+;;(setq doom-theme 'doom-vibrant) ;; DEFAULT THEME
+(setq doom-theme 'doom-dark+)
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
@@ -76,6 +77,7 @@
 
 ;; LaTeX function and configs:
 (after! latex
+  (setq-default TeX-engine 'xetex)
   (setq TeX-save-query nil))
 
 (defun TeX-compile ()
