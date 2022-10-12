f9fc203f5cf1c31933fee9b0842d3cbc8de8bd07
dreydengys <busterkeel59@gmail.com>
2022-10-12 22:36:23 +0200

    Add new package to doom
     - deft: managing easily notes
     - org-roam: linking files between each others in org mode

--- init.el
+++ init.el
@@ -25,7 +25,7 @@
        ivy               ; a search engine for love and life
 
        :ui
-       ;;deft              ; notational velocity for Emacs
+       deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
@@ -144,7 +144,8 @@
        (org                ; organize your plain life in plain text
         +pretty
         +dragndrop
-        +present)
+        +present
+        +roam)
        php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
